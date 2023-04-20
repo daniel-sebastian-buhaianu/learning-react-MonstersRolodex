9adab6f096b98f41ceae2e3fbb08505a84d9523a
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-04-20 10:33:54 +0300

    fetched users and displayed them

--- src/App.js
+++ src/App.js
@@ -7,13 +7,28 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      monsters: [],
     }
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
   }
 
   render() {
     return (
       <div className="App">
-        
+        {
+          this.state.monsters.map(monster => {
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
+            )
+          })
+        }
       </div>
     );
   }
