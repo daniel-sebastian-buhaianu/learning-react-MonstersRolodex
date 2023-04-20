0f64d0e821f75a2faae8e5ec830b2ef07d5f10ba
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-04-20 14:28:14 +0300

    created card component and added styles for all components

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
     return (
       <div className='App'>
         <SearchBox
-          className='search-box' 
+          className='monsters-search-box' 
           placeholder='search monsters' 
           onChange={event => this.setState({ searchInput: event.target.value })}
         />
